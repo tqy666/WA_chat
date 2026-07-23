26501e38ce015436e1db6f6a05a6b68cf7e914e0
Liuliu <1048079943@qq.com>
2026-07-24 01:57:34 +0800

    去掉了x-api-key校验

--- app/api/chat.py
+++ app/api/chat.py
@@ -18,12 +18,12 @@ async def handle_webhook(request: Request):
     body = await request.json()
     print("收到的数据:", body)
 
-    api_key = request.headers.get("x-api-key")
-    if not api_key or api_key !=  os.getenv("API_MASTER_KEY"):
-        return JSONResponse(
-            status_code=403,
-            content={"status": "error", "message": "Invalid API Key"}
-        )
+    # api_key = request.headers.get("x-api-key")
+    # if not api_key or api_key !=  os.getenv("API_MASTER_KEY"):
+    #     return JSONResponse(
+    #         status_code=403,
+    #         content={"status": "error", "message": "Invalid API Key"}
+    #     )
 
     headers = dict(request.headers)
     print(f"打印头部消息：{headers}")
